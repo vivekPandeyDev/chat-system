0bb1779d267640b3ffa2addfc4b5d2e6b115e8a8
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-10-01 20:12:13 +0530

    initial spring boot chat application setup

---
+++ src/test/java/com/loop/troop/chat/ChatSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loop.troop.chat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class ChatSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	    log.info("Spring boot test context loaded");
+    }
+
+}
